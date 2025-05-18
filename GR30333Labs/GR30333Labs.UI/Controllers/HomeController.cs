41568759bd5e7aa0137a961552216db6bc619ed3
brauck <109923715+brauck@users.noreply.github.com>
2025-05-18 08:20:27 +0300

    start web

---
+++ GR30333Labs/GR30333Labs.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GR30333Labs.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GR30333Labs/GR30333Labs.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GR30333Labs.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
