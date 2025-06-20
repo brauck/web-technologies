2cc907fa3250cffc1671e39234e80f6317825dd6
brauck <109923715+brauck@users.noreply.github.com>
2025-06-20 15:53:23 +0300

    finish lab2 partial

---
+++ Kalevich/Kalevich.UI/wwwroot/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kalevich.UI.wwwroot.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            retun View();
+        }
+    }
+}
