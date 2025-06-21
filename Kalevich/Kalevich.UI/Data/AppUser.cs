95127c2d4ec9a1b013519365a5f516975861c395
brauck <109923715+brauck@users.noreply.github.com>
2025-06-21 15:10:37 +0300

    start lab3

---
+++ Kalevich/Kalevich.UI/Data/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Kalevich.UI.Data
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
--- Kalevich/Kalevich.UI/Data/ApplicationDbContext.cs
+++ Kalevich/Kalevich.UI/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Kalevich.UI.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
